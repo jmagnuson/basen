380a34143cad79e89ab7069f8bb283aee37d2252
jmagnuson <magnu225@umn.edu>
2016-09-05 18:03:36 -0500

    Fix formatting

--- src/basen.rs
+++ src/basen.rs
@@ -10,30 +10,38 @@ use std::cmp::Ordering;
 #[derive(Debug, Eq, Clone)]
 pub struct BaseN {
     pub base: usize,
-    pub vec: Vec<u8>
+    pub vec: Vec<u8>,
 }
 
 impl BaseN {
-
     /// Creates new number given a base.
     pub fn new(base: usize) -> BaseN {
-        BaseN { base: base, vec: Vec::new() }
+        BaseN {
+            base: base,
+            vec: Vec::new(),
+        }
     }
 
     /// Creates new number with initial vector capacity.
     pub fn with_capacity(base: usize, capacity: usize) -> BaseN {
-        BaseN { base: base, vec: Vec::with_capacity(capacity) }
+        BaseN {
+            base: base,
+            vec: Vec::with_capacity(capacity),
+        }
     }
 
     /// Creates new number with existing base and vec.
     pub fn with_existing(base: usize, vec: Vec<u8>) -> BaseN {
-        BaseN { base: base, vec: vec }
+        BaseN {
+            base: base,
+            vec: vec,
+        }
     }
 
     /// Creates a new number from base and usize value.
     pub fn from_usize(base: usize, val: usize) -> Result<BaseN, &'static str> {
 
-        let mut new_vec:Vec<u8> = Vec::new();
+        let mut new_vec: Vec<u8> = Vec::new();
         let mut val10: usize = val;
         loop {
             let val = val10 / base;
@@ -49,7 +57,10 @@ impl BaseN {
                 }
             }
         }
-        let new_basen: BaseN = BaseN { base: base, vec: new_vec };
+        let new_basen: BaseN = BaseN {
+            base: base,
+            vec: new_vec,
+        };
 
         Ok(new_basen)
     }
@@ -88,7 +99,7 @@ impl BaseN {
     /// Converts to a new base copy.
     pub fn to_base(&self, new_base: usize) -> Result<BaseN, &'static str> {
 
-        //TODO: If base is same, just return existing??
+        // TODO: If base is same, just return existing??
         if self.base == new_base {
             return Ok(self.clone());
         }
@@ -98,22 +109,20 @@ impl BaseN {
         let val10: usize = self.to_usize().unwrap();
 
         BaseN::from_usize(new_base, val10)
-        
+
     }
 
     /// Converts existing BaseN to a usize.
     pub fn to_usize(&self) -> Result<usize, &'static str> {
         let mut val10: usize = 0;
         for (i, x) in self.vec.iter().enumerate() {
-            val10 = val10 + (*x as usize) * self.base.pow(i as u32);
+            val10 += (*x as usize) * self.base.pow(i as u32);
         }
         Ok(val10)
     }
-
 }
 
 impl PartialEq for BaseN {
-
     /// Compares partial equivalence between the values of arbitrary bases.
     fn eq(&self, other: &BaseN) -> bool {
         if self.base == other.base {
@@ -125,7 +134,6 @@ impl PartialEq for BaseN {
 }
 
 impl PartialOrd for BaseN {
-
     /// Compares partial order between the values of arbitrary bases.
     fn partial_cmp(&self, other: &BaseN) -> Option<Ordering> {
         Some(self.cmp(other))
@@ -133,7 +141,6 @@ impl PartialOrd for BaseN {
 }
 
 impl Ord for BaseN {
-
     /// Compares total order between the values of arbitrary bases.
     fn cmp(&self, other: &BaseN) -> Ordering {
         self.to_usize().unwrap().cmp(&other.to_usize().unwrap())
@@ -147,11 +154,11 @@ impl Add<BaseN> for BaseN {
     // I suppose you could add a base16 to base10 with an internal convert
     fn add(self, rhs: BaseN) -> Result<BaseN, &'static str> {
 
-        let add_basen = if rhs.base==self.base {
+        let add_basen = if rhs.base == self.base {
             rhs
-        } else { 
+        } else {
             rhs.to_base(self.base).unwrap()
-            // TODO: Throw error?  Don't actually know 
+            // TODO: Throw error?  Don't actually know
             // what base user wanted to end with
         };
 
@@ -167,19 +174,22 @@ impl Add<BaseN> for BaseN {
                     let sum: u8 = *x + *y + carry;
                     carry = sum / (add_basen.base as u8);
                     new_vec.push(sum % (add_basen.base as u8));
-                },
+                }
                 (Some(x), None) => {
                     // no carry, but need to resize
                     let sum: u8 = *x + carry;
                     carry = sum / (add_basen.base as u8);
                     new_vec.push(sum % (add_basen.base as u8));
-                },
-                (_, _) => break // no ops needed
+                }
+                (_, _) => break, // no ops needed
 
             }
         }
 
-        let new_basen: BaseN = BaseN { base: add_basen.base, vec: new_vec };
+        let new_basen: BaseN = BaseN {
+            base: add_basen.base,
+            vec: new_vec,
+        };
 
         Ok(new_basen)
     }
@@ -200,11 +210,11 @@ impl Sub for BaseN {
     // I suppose you could add a base16 to base10 with an internal convert
     fn sub(self, rhs: BaseN) -> Result<BaseN, &'static str> {
 
-        let sub_basen = if rhs.base==self.base {
+        let sub_basen = if rhs.base == self.base {
             rhs
-        } else { 
+        } else {
             rhs.to_base(self.base).unwrap()
-            // TODO: Throw error?  Don't actually know 
+            // TODO: Throw error?  Don't actually know
             // what base user wanted to end with
         };
 
@@ -217,22 +227,33 @@ impl Sub for BaseN {
             match (self.vec.get(i), sub_basen.vec.get(i)) {
                 (Some(x), Some(y)) => {
                     let mut diff: i8 = (*x as i8) - (*y as i8) - (borrow as i8);
-                    borrow = if diff < 0 { 
-                        diff += self.base as i8; 1 } else { 0 };
+                    borrow = if diff < 0 {
+                        diff += self.base as i8;
+                        1
+                    } else {
+                        0
+                    };
                     new_vec.push(diff as u8);
-                },
+                }
                 (Some(x), None) => {
                     let mut diff: i8 = (*x as i8) - (borrow as i8);
-                    borrow = if diff < 0 { 
-                        diff += self.base as i8; 1 } else { 0 };
+                    borrow = if diff < 0 {
+                        diff += self.base as i8;
+                        1
+                    } else {
+                        0
+                    };
                     new_vec.push(diff as u8);
-                },
-                (_, _) => break // no ops needed
+                }
+                (_, _) => break, // no ops needed
 
             }
         }
 
-        let new_basen: BaseN = BaseN { base: sub_basen.base, vec: new_vec };
+        let new_basen: BaseN = BaseN {
+            base: sub_basen.base,
+            vec: new_vec,
+        };
 
         Ok(new_basen)
     }
@@ -243,7 +264,8 @@ impl Mul for BaseN {
 
     fn mul(self, rhs: BaseN) -> Result<BaseN, &'static str> {
         // XXX: convert to usize for now
-        BaseN::from_usize(self.base, self.to_usize().unwrap() * rhs.to_usize().unwrap())
+        BaseN::from_usize(self.base,
+                          self.to_usize().unwrap() * rhs.to_usize().unwrap())
     }
 }
 
@@ -252,6 +274,7 @@ impl Div for BaseN {
 
     fn div(self, rhs: BaseN) -> Result<BaseN, &'static str> {
         // XXX: convert to usize for now
-        BaseN::from_usize(self.base, self.to_usize().unwrap() / rhs.to_usize().unwrap())
+        BaseN::from_usize(self.base,
+                          self.to_usize().unwrap() / rhs.to_usize().unwrap())
     }
 }
